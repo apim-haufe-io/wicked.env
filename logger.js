14a6fb9b1e482fab41e143145f625be9fa60f469
Martin Danielsson <martin@danielsson.de>
2018-05-15 19:35:47 +0200

    Enable logging plain text, e.g. when running tests

--- logger.js
+++ logger.js
@@ -7,17 +7,22 @@ if (process.env.LOG_LEVEL) {
     logLevel = process.env.LOG_LEVEL;
 }
 
-const consoleLogger = winston.createLogger({
-    level: logLevel,
-    transports: [new winston.transports.Console()]
-});
-
-function makeLog(moduleName, log) {
-    return {
-        date: new Date().toISOString(),
-        module: moduleName,
-        message: log
-    };
+let consoleLogger;
+let makeLog;
+if (!process.env.LOG_PLAIN) {
+    makeLog = makeLogJson;
+    consoleLogger = winston.createLogger({
+        level: logLevel,
+        format: winston.format.json(),
+        transports: [new winston.transports.Console()]
+    });
+} else {
+    makeLog = makeLogPlain;
+    consoleLogger = winston.createLogger({
+        level: logLevel,
+        format: winston.format.simple(),
+        transports: [new winston.transports.Console()]
+    });
 }
 
 consoleLogger.info(makeLog('portal-env:logger', `Setting up logging with log level "${logLevel}" (override with LOG_LEVEL)`));
@@ -41,5 +46,34 @@ const logger = (moduleName) => {
         }
     };
 };
+
+// Log formatters
+
+// makeLogPlain makes debugging unit tests a lot easier
+function makeLogPlain(moduleName, log) {
+    let s = log;
+    if (log && typeof(log) !== 'string')
+        s = JSON.stringify(log);
+    if (log === undefined)
+        s = '(undefined)';
+    else if (log === null)
+        s = '(null)';
+    // Special case for logging Error instances
+    if (log && log.stack) {
+        console.error(log);
+    }
+    return `${moduleName.padEnd(30)} ${s}`;
+}
+
+// This is the standard logger which always outputs a structured JSON
+// log.
+function makeLogJson(moduleName, log) {
+    // level is not used here
+    return {
+        date: new Date().toISOString(),
+        module: moduleName,
+        message: log
+    };
+}
 
 module.exports = logger;
